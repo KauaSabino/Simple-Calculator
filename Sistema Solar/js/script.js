8507ef18efe6e9ee3d046fdd23cdb538802cc702
Kaua Sabino <sabinokaua30@gmail.com>
2021-11-10 14:11:54 -0300

    commit project

---
+++ Sistema Solar/js/script.js
@@ -0,0 +1,188 @@
+//MERCURIO
+document.getElementById("orbit_mercury").animate([
+  // keyframes
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+  // timing options
+  duration: 5000,
+  iterations: Infinity
+});
+
+document.getElementById("mercury").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 5000,
+  iterations: Infinity
+});
+
+//VÊNUS
+document.getElementById("orbit_venus").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 8000,
+  iterations: Infinity
+});
+
+document.getElementById("venus").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+  duration: 8000,
+  iterations: Infinity
+});
+
+//TERRA
+document.getElementById("orbit_earth").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 10000,
+  iterations: Infinity
+});
+
+document.getElementById("earth").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 10000,
+  iterations: Infinity
+});
+
+//LUA
+document.getElementById("moon").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 10000,
+  iterations: Infinity
+});
+
+//MARTE
+document.getElementById("orbit_mars").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 12000,
+  iterations: Infinity
+});
+
+document.getElementById("mars").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 12000,
+  iterations: Infinity
+});
+
+//ASTEROIDES
+document.getElementById("orbit_asteroid").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 20000,
+  iterations: Infinity
+});
+
+//JUPITER
+document.getElementById("orbit_jupiter").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 26000,
+  iterations: Infinity
+});
+
+document.getElementById("jupiter").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 26000,
+  iterations: Infinity
+});
+
+//SATURNO
+document.getElementById("orbit_saturn").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 30000,
+  iterations: Infinity
+});
+
+document.getElementById("saturn").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 30000,
+  iterations: Infinity
+});
+
+//URANO
+document.getElementById("orbit_uranus").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 36000,
+  iterations: Infinity
+});
+
+document.getElementById("uranus").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 36000,
+  iterations: Infinity
+});
+
+//NETUNO
+document.getElementById("orbit_neptune").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(360deg)' }
+], {
+
+  duration: 40000,
+  iterations: Infinity
+});
+
+document.getElementById("neptune").animate([
+
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(-360deg)' }
+], {
+
+  duration: 40000,
+  iterations: Infinity
+});
